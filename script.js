3f1ccef226d0ad480c2802e4fb1b77f0da9ad30d
Alejandro-1-Sosa <141190092+Alejandro-1-Sosa@users.noreply.github.com>
2025-05-31 17:26:41 -0300

    commit

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ function seleccionar(){
 
 //Funcion que aplica las animaciones de las habilidades
 function efectoHabilidades(){
-    var skills = document.getElementById("skills");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    let skills = document.getElementById("skills");
+    let distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
         habilidades[0].classList.add("javascript");
