6df762ec6cba5d45f34921c29d52d09b1c38920e
Alejandro-1-Sosa <141190092+Alejandro-1-Sosa@users.noreply.github.com>
2025-10-08 17:13:27 -0300

    commit

--- script.js
+++ script.js
@@ -40,41 +40,5 @@ window.onscroll = function(){
 } 
 
 // Muestra una alerta y envia un mensaje al presionar el botón de Enviar 
-const form = document.getElementById('form-contacto');
-const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
 
-const appendAlert = (message, type) => {
-  const wrapper = document.createElement('div')
-  wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    '</div>'
-  ].join('')
-
-  alertPlaceholder.append(wrapper)
-}
-
-form.addEventListener('submit',function (e) {
-    e.preventDefault(); // evita que recargue la página
-
-    const datos = new FormData(form);
-
-    fetch(form.action, {
-        method: 'POST',
-        body: datos,
-        headers: {
-            'Accept': 'application/json'
-        }
-    }).then(response => {
-        if (response.ok) {
-            alert('El mensaje ha sido enviado con éxito ✅','success');
-            form.reset();
-        } else {
-            alert('Hubo un error. Intenta nuevamente ❌','danger');
-        }
-    }).catch(()=> {
-        appendAlert('Error en la conexión. Vuelve a intentarlo ⚠️','warning')
-    })
-});
 
